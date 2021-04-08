0125076968a1ecfeb9594145148136a1674f08b4
Sandeep Mehrotra <sandeep2.mehrotra@gmail.com>
2021-04-08 15:06:30 +0000

    Testing for demo

--- sample-master/internal/server.js
+++ sample-master/internal/server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 const mockEvents = {
     events: [
         { title: 'an event', id: 1, description: 'something really cool' },
+        { title: 'another event', id: 2, description: 'something even cooler' },
         { title: 'another event', id: 2, description: 'something even cooler' }
     ]
 };
